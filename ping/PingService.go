ccc5a4c0abfad034dbe71e374b2a622e21ff9dd7
zhang hailong <hailongz@qq.com>
2016-12-06 11:33:02 +0800

    update

--- ping/PingService.go
+++ ping/PingService.go
@@ -11,54 +11,157 @@ import (
 
 type PingService struct {
 	app.Service
-	Options        map[string]interface{}
-	Expires        int64
-	ToName         string
+	Init           *app.InitTask
 	ReceiveMessage *remote.RemoteReceiveMessageTask
+	Query          *PingQueryTask
+	Cleanup        *PingCleanupTask
 
-	address string
+	dispatch *kk.Dispatch
+	pings    map[int64]Ping
+	id       int64
 }
 
 func (S *PingService) Handle(a app.IApp, task app.ITask) error {
 	return app.ServiceReflectHandle(a, task, S)
 }
 
-func (S *PingService) Ping(a app.IApp) error {
+func (S *PingService) HandleInitTask(a app.IApp, task *app.InitTask) error {
 
-	task := remote.RemoteSendMessageTask{}
+	S.dispatch = kk.NewDispatch()
+	S.pings = map[int64]Ping{}
+	S.id = 0
 
-	task.Message.Method = "PING"
-	task.Message.To = S.ToName
-	task.Message.Type = "text/json"
-
-	var v = Ping{}
-
-	v.Options = S.Options
-	v.Address = S.address
-
-	task.Message.Content, _ = json.Encode(&v)
-
-	kk.GetDispatchMain().AsyncDelay(func() {
-		S.Ping(a)
-	}, time.Duration(S.Expires)*time.Second)
-
-	log.Println("PING")
-	log.Println(v)
-
-	return app.Handle(a, &task)
+	return nil
 }
 
 func (S *PingService) HandleRemoteReceiveMessageTask(a app.IApp, task *remote.RemoteReceiveMessageTask) error {
 
-	if task.Message.Method == "CONNECTED" {
+	log.Println(task.Message.String())
 
-		S.address = string(task.Message.Content)
+	if task.Message.Method == "PING" {
 
-		kk.GetDispatchMain().Async(func() {
-			S.Ping(a)
-		})
+		var v = Ping{}
+
+		err := json.Decode(task.Message.Content, &v)
+
+		if err == nil {
+
+			v.Name = task.Message.From
+			v.Atime = time.Now().Unix()
+			v.Status = PingStatusOnline
+
+			S.dispatch.Async(func() {
+
+				for _, ping := range S.pings {
+
+					if ping.Name == v.Name && ping.Address == v.Address {
+						ping.Atime = v.Atime
+						ping.Status = v.Status
+						if v.Options != nil {
+							ping.Options = v.Options
+						}
+						if v.Counter != nil {
+							ping.Counter = v.Counter
+						}
+						return
+					}
+
+				}
+
+				S.pings[S.id] = v
+
+				S.id = S.id + 1
+
+			})
+
+		} else {
+			log.Println("[PingService] " + err.Error())
+		}
+
+	} else if task.Message.Method == "PING.DISCONNECTED" {
+
+		var v = Ping{}
+
+		err := json.Decode(task.Message.Content, &v)
+
+		if err == nil {
+
+			v.Name = task.Message.From
+			v.Atime = time.Now().Unix()
+			v.Status = PingStatusOffline
+
+			S.dispatch.Async(func() {
+
+				for _, ping := range S.pings {
+
+					if ping.Name == v.Name && ping.Address == v.Address {
+						ping.Atime = v.Atime
+						ping.Status = v.Status
+						if v.Options != nil {
+							ping.Options = v.Options
+						}
+						if v.Counter != nil {
+							ping.Counter = v.Counter
+						}
+						return
+					}
+
+				}
+
+			})
+
+		} else {
+			log.Println("[PingService] " + err.Error())
+		}
 
 	}
+
+	return nil
+}
+
+func (S *PingService) HandlePingQueryTask(a app.IApp, task *PingQueryTask) error {
+
+	S.dispatch.Sync(func() {
+
+		var pings = []Ping{}
+
+		for _, ping := range S.pings {
+
+			if (task.Name == "" || ping.Name == task.Name) && (task.Address == "" || ping.Address == task.Address) {
+				pings = append(pings, ping)
+			}
+
+		}
+
+		task.Result.Pings = pings
+
+	})
+
+	return nil
+}
+
+func (S *PingService) HandlePingCleanupTask(a app.IApp, task *PingCleanupTask) error {
+
+	S.dispatch.Sync(func() {
+
+		var ids = []int64{}
+		var now = time.Now().Unix()
+
+		for id, ping := range S.pings {
+
+			if ping.Status == PingStatusOffline || ping.Atime+task.Expires < now {
+				ids = append(ids, id)
+			}
+
+		}
+
+		for _, id := range ids {
+			delete(S.pings, id)
+		}
+
+		log.Println("[PingService][HandlePingCleanupTask] ", ids)
+
+	})
 
 	return nil
 }
--- ping/ping.go
+++ ping/ping.go
@@ -5,15 +5,20 @@ import (
 	"github.com/kkserver/kk-lib/kk/app/remote"
 )
 
+const PingStatusOnline = 200
+const PingStatusOffline = 300
+
 type Ping struct {
 	Name    string                 `json:"name,omitempty"`
 	Options map[string]interface{} `json:"options,omitempty"`
 	Address string                 `json:"address,omitempty"`
+	Counter *remote.Counter        `json:"counter,omitempty"`
+	Status  int                    `json:"status,omitempty"`
 	Atime   int64                  `json:"atime,omitempty"`
 }
 
 type PingApp struct {
 	app.App
 	Remote *remote.Service
-	Ping   *PingQueryService
+	Ping   *PingService
 }
